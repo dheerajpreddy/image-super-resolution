eedea509086fffa84104da62d57661185977e61e
Kshitij Gupta <kgupta119@gmail.com>
2019-03-02 06:22:31 +0530

    Added SRCNN model

---
+++ model.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+
+class SRCNN(nn.Module):
+	def __init__(self):
+		super(SRCNN, self).__init__()
+
+		# Patch extraction and representation
+		self.conv1 = nn.Conv2d(3, 64, kernel_size=9, padding=4)
+		self.relu1 = nn.ReLU()
+
+		# Non linear mapping
+		self.conv2 = nn.Conv2d(64, 32, kernel_size=1, padding=0)
+		self.relu2 = nn.ReLU()
+
+		# Reconstruction
+		self.conv3 = nn.Conv2d(32, 3, kernel_size=5, padding=2)
+
+	def forward(self, x):
+		out = self.conv1(x)
+		out = self.relu1(out)
+		out = self.conv2(out)
+		out = self.relu2(out)
+		out = self.conv3(out)
+
+		return out
